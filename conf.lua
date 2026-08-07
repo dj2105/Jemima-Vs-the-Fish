f166a1c1670d1a62a31ea4f29a75dde05e014bbd
dj2105 <54191462+dj2105@users.noreply.github.com>
2026-08-07 23:16:59 +0100

    Add Love2D project configuration

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.identity = "jemima-vs-the-fish"
+    t.version = "11.5"
+    t.console = false
+    t.window.title = "Jemima vs the Fish"
+    t.window.width = 640
+    t.window.height = 480
+    t.window.resizable = true
+    t.window.vsync = 1
+end
